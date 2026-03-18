020b6093a0e85c65fa5886caea94b16d4da1a5ca
Alex Maharjan <153304666+Lxmhrjn@users.noreply.github.com>
2026-03-18 17:41:59 +0545

    Added by Alex

--- edit_validation.php
+++ edit_validation.php
@@ -3,13 +3,13 @@ include 'db.php';
 include 'layout.php';
 
 if (isset($_POST['update'])) {
-    $id = $_POST['id']; // Gets hidden field value
+    $id = $_POST['id']; 
     $model = trim($_POST['model']);
     $brand = trim($_POST['brand']);
     $price = trim($_POST['price']);
     $category = trim($_POST['category']);
 
-    // Validate inputs
+    
     if (empty($model) || empty($brand) || empty($price) || empty($category)) {
         $error = "All fields are required.";
         header("Location: vehicle_edit.php?id=$id&error=" . urlencode($error));
@@ -19,7 +19,7 @@ if (isset($_POST['update'])) {
         header("Location: vehicle_edit.php?id=$id&error=" . urlencode($error));
         exit;
     } else {
-        // Query to update the specific record using id
+       
         $query = "UPDATE vehicles SET 
                     model='$model', 
                     brand='$brand', 
@@ -27,12 +27,12 @@ if (isset($_POST['update'])) {
                     category='$category' 
                   WHERE id='$id'";
         
-        // Execute the update
+        
         if (mysqli_query($conn, $query)) {
             echo "<h3>Vehicle updated successfully!</h3>";
             echo "<br><a href='main.php' class='btn btn-primary'>Go to Home</a>";
             include 'footer.php';
-            exit; // Stop executing rest of script on success
+            exit; 
         } else {
             $error = "Error updating vehicle: " . mysqli_error($conn);
             header("Location: vehicle_edit.php?id=$id&error=" . urlencode($error));
